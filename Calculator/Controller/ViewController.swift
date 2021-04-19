1ce2993f9c42be63e8952ed1981cfeea45f54543
Michael Einman <einmanjr@gmail.com>
2021-04-19 16:46:42 -0700

    First Commit

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Michael Einman on 4/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func acButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func posnegButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func percentButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func divisionButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func sevenButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func eightButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func ninebutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiplicationButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func fourButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func fiveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func sixButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func minusButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
