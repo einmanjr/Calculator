9f3fb96796f76cc009919b04e6ed4c33e4cd73b5
Michael Einman <einmanjr@gmail.com>
2021-04-20 15:49:46 -0700

    trying to implement consolidation of numbers and operators buttons.

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var calculation = Calculation()
 
     
     
@@ -19,92 +20,47 @@ class ViewController: UIViewController {
         calculationString = ""
     }
     
-    @IBAction func posnegButton(_ sender: UIButton) {
-        addCalc(pressed: "-")
-        //THIS IS PROBABLY FUCKED!!
-    }
-    
-    @IBAction func percentButton(_ sender: UIButton) {
-        addCalc(pressed: "%")
-    }
-    
-    @IBAction func divisionButton(_ sender: UIButton) {
-        addCalc(pressed: "/")
-    }
-    
-    @IBAction func sevenButton(_ sender: UIButton) {
-        addCalc(pressed: "7")
-    }
-    
-    @IBAction func eightButton(_ sender: UIButton) {
-        addCalc(pressed: "8")
-    }
-    
-    @IBAction func ninebutton(_ sender: UIButton) {
-        addCalc(pressed: "9")
-    }
-    
-    @IBAction func multiplicationButton(_ sender: UIButton) {
-        addCalc(pressed: "*")
-    }
-    
-    @IBAction func fourButton(_ sender: UIButton) {
-        addCalc(pressed: "4")
-    }
-    
-    @IBAction func fiveButton(_ sender: UIButton) {
-        addCalc(pressed: "5")
-    }
-    
-    @IBAction func sixButton(_ sender: UIButton) {
-        addCalc(pressed: "6")
-    }
-    
-    @IBAction func minusButton(_ sender: UIButton) {
-        addCalc(pressed: "-")
-    }
-    
-    @IBAction func oneButton(_ sender: UIButton) {
-        addCalc(pressed: "1")
-    }
-    
-    @IBAction func twoButton(_ sender: UIButton) {
-        addCalc(pressed: "2")
-    }
-    
-    @IBAction func threeButton(_ sender: UIButton) {
-        addCalc(pressed: "3")
-    }
-    
-    @IBAction func plusButton(_ sender: UIButton) {
-        addCalc(pressed: "+")
-    }
-    
-    @IBAction func zeroButton(_ sender: UIButton) {
-        addCalc(pressed: "0")
-    }
-    
-    @IBAction func decimalButton(_ sender: UIButton) {
-        addCalc(pressed: ".")
-    }
-    
-    @IBAction func equalsButton(_ sender: UIButton) {
-        calculate()
-    }
-    
-    
-    var calculationString: String = ""
-    
-    func addCalc (pressed: String){
+
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let pressed = sender.titleLabel?.text!
         calculationString.append("\(pressed)")
         resultLabelUpdate()
         print("\(pressed)")
     }
     
+
+    
+    @IBAction func operatorPressed(_ sender: UIButton) {
+        let pressed = sender.titleLabel?.text!
+        calculationString.append(pressed!)
+        print("\(pressed)")
+    }
+    
+
+    
+    @IBAction func equalsButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    var calculationString: String = ""
+    
+//    func numberPressed (pressed: String){
+//        calculationString.append("\(pressed)")
+//        resultLabelUpdate()
+//        print("\(pressed)")
+//    }
+//
+//    func operationPressed (pressed: String) {
+//        calculationString.append(pressed)
+//        print("\(pressed)")
+//    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        calculate()
+//        calculate()
     }
     
     func resultLabelUpdate(){
@@ -112,10 +68,10 @@ class ViewController: UIViewController {
         resultLabel.text = calculationString
     }
     
-    func calculate() -> Double{
-        let result = Double(calculationString) ?? 0
-        return result
-    }
+//    func calculate() -> Double{
+//        let result = Double(calculationString) ?? 0
+//        return result
+//    }
     
 }
 
