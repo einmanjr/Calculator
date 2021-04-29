cae998f0ec135ceb19827ce9be2e204239a360a7
Michael Einman <einmanjr@gmail.com>
2021-04-29 16:44:57 -0700

    Num String concatenation	for numbers success.

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var isFinishedTypingNumber: Bool = true
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func calcButtonPressed(_ sender: Any) {
+        isFinishedTypingNumber = true
+        
     }
-    
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle{
+            
+            if isFinishedTypingNumber == true {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
-    
 }
-
