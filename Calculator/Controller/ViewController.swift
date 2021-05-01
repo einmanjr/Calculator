7d23f8008ffae4cb10ebf2703b5a3b88162e59a1
Michael Einman <einmanjr@gmail.com>
2021-04-30 17:08:50 -0700

    calc operators added functionality.. top row only

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -9,23 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isFinishedTypingNumber: Bool = true
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double{
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot CONVERT displayLabel.text into a DOUBLE")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    @IBAction func calcButtonPressed(_ sender: Any) {
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
         
+
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue *= -1
+            } else if calcMethod == "AC" {
+                displayValue = 0
+            } else if calcMethod == "%" {
+                displayValue /= 100
+            }
+        }
+        
     }
+    
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        
         if let numValue = sender.currentTitle{
             
             if isFinishedTypingNumber == true {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
     }
 }
+
